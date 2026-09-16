06e954b0bd8c445f6ccfd37cd1c0a3cbe0503d4b
Yeoboe <yeoboe7@gmail.com>
2026-09-17 00:11:48 +0300

    Yeoboe XMD update

---
+++ settings.js
@@ -0,0 +1,25 @@
+
+/**
+* Yeoboe x
+* Yeoboe x
+*/
+
+const settings = {
+  packname: 'Yeoboe',
+  author: 'Yeoboe',
+  botName: "Yeoboe-xmd",
+  botOwner: 'Yeoboe', // Your name
+  ownerNumber: process.env.OWNER_NUMBER || '255742579250', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.7.6",
+  updateBranch: 'main',
+  updateZipToken: '',
+  updateZipUrl: "https://github.com/Yeoboe/Yeoboe-xmd/archive/refs/heads/main.zip",
+  timezone: 'Africa/nairobi',
+};
+
+module.exports = settings;
